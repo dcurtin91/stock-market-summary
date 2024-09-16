458da515b777727180964e460ee38e3f76d10cf8
dcurtin91 <109159085+dcurtin91@users.noreply.github.com>
2024-09-16 18:35:12 -0500

    Update Data.tsx

--- stock-market-summary/src/Data.tsx
+++ stock-market-summary/src/Data.tsx
@@ -1,11 +1,5 @@
 import { useEffect, useState } from 'react';
 
-interface StockData {
-    summary: string;
-    sectors: string;
-    top_gainers: Gainer[];
-    top_losers: Loser[];
-}
 
 interface Gainer {
     ticker: string;
@@ -21,35 +15,46 @@ interface Loser {
 
 function Data() {
     
-    const [stockData, setStockData] = useState<StockData | null>(null);
+    const [summary, setSummary] = useState<string>('');
+    const [sectors, setSectors] = useState<string>('');
+    const [topGainers, setTopGainers] = useState<Gainer[]>([]);
+    const [topLosers, setTopLosers] = useState<Loser[]>([]);
+
     const url = 'https://stock-market-summary-server-b48b85a337b0.herokuapp.com/summarize-market';
 
     useEffect(() => {
         fetch(url)
         .then(response => response.json())
         .then(data => {
-            setStockData(data);
+            const dataSummary = data.summary;
+            const dataSectors = data.sectors;
+            const dataTopGainers = data.top_gainers;
+            const dataTopLosers = data.top_losers;
+            setSummary(dataSummary);
+            setSectors(dataSectors);
+            setTopGainers(dataTopGainers);
+            setTopLosers(dataTopLosers);
         })
         .catch(error => {
             console.error("Error parsing response: ", error);
         });
     }, [url]);
 
-    if (!stockData) {
+    if (!summary) {
         return <>Loading...</>;
     }
 
     return (
         <div>
            <h2>Today's Summary</h2>
-           <p>{stockData.summary}</p>
+           <p>{summary}</p>
            <h3>Notable Sector Performances</h3>
-           <p>{stockData.sectors}</p>
+           <p>{sectors}</p>
 
            <h3>Top Gainers</h3>
             <ul>
-                {stockData.top_gainers?.length ? (
-                    stockData.top_gainers.map((gainer, index) => (
+                {topGainers?.length ? (
+                    topGainers.map((gainer, index) => (
                         <li key={index}>
                             <strong>{gainer.ticker}</strong>: ${gainer.price} ({gainer.change_percent} change)
                         </li>
@@ -61,8 +66,8 @@ function Data() {
 
             <h3>Top Losers</h3>
             <ul>
-                {stockData.top_losers?.length ? (
-                    stockData.top_losers.map((loser, index) => (
+                {topLosers?.length ? (
+                    topLosers.map((loser, index) => (
                         <li key={index}>
                             <strong>{loser.ticker}</strong>: ${loser.price} ({loser.change_percent} change)
                         </li>
