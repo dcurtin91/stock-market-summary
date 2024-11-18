101468045a5a48d5962679db206010f4a4b3b68d
dcurtin91 <109159085+dcurtin91@users.noreply.github.com>
2024-11-17 18:54:41 -0600

    Update Firebase.tsx

--- stock-market-summary/src/Firebase.tsx
+++ stock-market-summary/src/Firebase.tsx
@@ -119,8 +119,8 @@ function RenderSummaries() {
                       <p>Ticker: <a href={`https://finance.yahoo.com/quote/${gainer.ticker}`} target="_blank" rel="noopener noreferrer">{gainer.ticker}</a></p>
                       <p>Price: ${gainer.price}</p>
                       <p>Volume: {gainer.volume.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
-                      <p>Percent Change: {gainer.change_percentage}</p>
-                      <p>Change Amount: ${gainer.change_amount.toLocaleString('en')}</p>
+                      <p>Change Percentage: {gainer.change_percentage}</p>
+                      <p>Total Change Amount: ${gainer.change_amount.toLocaleString('en')}</p>
                     </div>
                   ))
                 : <p>No gainers available</p>}
@@ -133,8 +133,8 @@ function RenderSummaries() {
                       <p>Ticker: <a href={`https://finance.yahoo.com/quote/${loser.ticker}`} target="_blank" rel="noopener noreferrer">{loser.ticker}</a></p>
                       <p>Price: ${loser.price}</p>
                       <p>Volume: {loser.volume.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
-                      <p>Percent Change: {loser.change_percentage}</p>
-                      <p>Change Amount: <span className="negativeCurr">{loser.change_amount.toString().replace(/-/g, "")}</span></p>
+                      <p>Change Percentage: {loser.change_percentage}</p>
+                      <p>Total Change Amount: <span className="negativeCurr">{loser.change_amount.toString().replace(/-/g, "")}</span></p>
                     </div>
                   ))
                 : <p>No losers available</p>}
