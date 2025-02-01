3de9f40d42fbd9ebc41d3542bed3c12275cb1f2b
dcurtin91 <109159085+dcurtin91@users.noreply.github.com>
2025-02-01 17:58:19 -0600

    Setting up table

--- stock-market-summary/src/RenderSummaries.tsx
+++ stock-market-summary/src/RenderSummaries.tsx
@@ -4,37 +4,57 @@ import MostActive from "./MostActive";
 import BubbleChart from "./BubbleChart";
 
 function RenderSummaries() {
+  const date = new Date();
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+  let currentDate = `${month}/${day}/${year}`;
   return (
     <div className="container">
-      <h2>Stock Market Summaries</h2>
-      <div className="container">
-      <div className="graph_div"><BubbleChart /></div>
-      <div className="table_div">
-        <table>
+      <h3>Stock Market Activity: {currentDate}</h3>
+      <div className="summaries">
+        <table className="full-width-table">
           <thead>
             <tr>
-              <th>Top Gainers</th>
               <th></th>
-              <th>Top Losers</th>
+              <th></th>
             </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>
-                  <TopGainers />
-                </td>
-                <td></td>
-                <td>
-                  <TopLosers />
-                </td>
-              </tr>
-            </tbody>
-          
+          </thead>
+          <tbody>
+            <tr>
+              <td className="half-width">
+                <div className="graph_div"><BubbleChart /></div>
+              </td>
+              <td className="half-width">
+                <div className="table_div">
+                  <table>
+                    <thead>
+                      <tr>
+                        <th>Top Gainers</th>
+                        <th>Top Losers</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>
+                          <TopGainers />
+                        </td>
+                        <td>
+                          <TopLosers />
+                        </td>
+                      </tr>
+                    </tbody>
+
+                  </table>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+
         </table>
       </div>
-      </div>
       <div> <MostActive /></div>
-      
+
     </div>
   );
 };
